5e2c79ff8bcec140b4e419babd54b5a4226237c9
sushantkhadka20xx <itsmesushant05@gmail.com>
2022-05-09 18:45:07 +0545

    Final Commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/style.css">
+    <title>Form to Php Data</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <form action="./mydata/mydata.php" method="POST">
+            <div class="header">
+                <h2>Registration Form</h2>
+            </div>
+            <div class="radioValue">
+                <div class="left">
+                    <h1>Account Type<sup>&nbsp;*</sup></h1>
+                    <input type="radio" name="Type" id="" value="Individual">Individual</input>
+                    <input type="radio" name="Type" id="" value="Minor">Minor</input>
+                </div>
+                <div class="right">
+                    <h1>Salutation</h1>
+                    <input type="radio" name="Sal" id="" value="Mr.">Mr.</input>
+                    <input type="radio" name="Sal" id="" value="Mrs.">Mrs.</input>
+                    <input type="radio" name="Sal" id="" value="M/S">M/S</input>
+                    <input type="radio" name="Sal" id="" value="Miss">Miss</input>
+                    <input type="radio" name="Sal" id="" value="Minor">Minor</input>
+                </div>
+            </div>
+            <div class="ActualData">
+                <div class="first">
+                    <label for="" class="data">First Name<sup>&nbsp;*</sup></label><br>
+                    <input type="text" name="fname" placeholder="First Name">
+                    <label for="" class="data">Mobile No<sup>&nbsp;*</sup></label><br>
+                    <input type="text" name="mobile" placeholder="Mobile No.">
+                    <label for="" class="data">Data of Birth(B.S)<sup>&nbsp;*</sup></label><br>
+                    <input type="date" name="datebs" placeholder="Data of Birth(B.S)">
+                </div>
+                <div class="second">
+                    <label for="" class="data">Middle Name<sup>&nbsp;*</sup></label><br>
+                    <input type="text" name="mname" placeholder="Middle Name">
+                    <label for="" class="data">Phone No<sup>&nbsp;*</sup></label><br>
+                    <input type="text" name="phone" placeholder="Phone No">
+                    <label for="" class="data">Data of Birth(A.D)<sup>&nbsp;*</sup></label><br>
+                    <input type="date" name="datead" placeholder="Data of Birth(A.D)">
+                    <button type="submit" name="submit">Download in Pdf</button>
+                </div>
+                <div class="third">
+                    <label for="" class="data">Last Name<sup>&nbsp;*</sup></label><br>
+                    <input type="text" name="lname" placeholder="Last Name">
+                    <label for="" class="data">Email<sup>&nbsp;*</sup></label><br>
+                    <input type="email" name="email" placeholder="Email">
+                    <label for="" class="data">Applying From the Country<sup>&nbsp;*</sup></label><br>
+                    <input type="text" id="country" placeholder="Nepal" disabled="disabled">
+                </div>
+            </div>
+    </div>
+    </form>
+    </div>
+</body>
+
+</html>
---
+++ mydata/mydata.php
@@ -0,0 +1,107 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['Type'])) {
+        $Account = $_POST['Type'];
+    }
+    if (isset($_POST['Sal'])) {
+        $Status = $_POST['Sal'];
+    }
+    $FirstName = $_POST['fname'];
+    $MiddleName = $_POST['mname'];
+    if ($MiddleName == null) {
+        $MiddleName = "Null";
+    }
+    $Mobile = $_POST['mobile'];
+    $Phone = $_POST['phone'];
+    $DateBS = $_POST['datebs'];
+    $DateAd = $_POST['datead'];
+    $LastName = $_POST['lname'];
+    $Email = $_POST['email'];
+    $Country = "Nepal";
+    require("/xampp/htdocs/php/FirstTry/fpdf/fpdf.php"); // includeing the file 
+    if ($FirstName == null || strlen($FirstName) < 5) {
+        header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+
+    } else {
+        if ($DateAd != null) {
+            if ($DateBS != null) {
+                if ($LastName != null) {
+                    if ($Email != null) {
+                        if ($Mobile == null || strlen($Mobile) < 10) {
+                            $alert = "<script>alert('Please Enter Your 10 Digit Mobile Number.');</script>";
+                        } else {
+                            if ($Phone == null || strlen($Phone) < 7) {
+                                header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+                            } else {
+                                if ($Account == null) {
+                                    header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+                                } else {
+                                    if ($Status == null) {
+                                        header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+                                    } else {
+                                        $pdf = new FPDF();
+                                        $pdf->AddPage();
+                                        $pdf->SetFont("Courier", "", 18);
+                                        // $pdf->Cell(0, 20, "Registration Details", 1, 1, 'C',false,'index.php');
+                                        /* 0 = width from the first 
+                                            13 = Height of the cell
+                                            1 = border 
+                                            1 = line for the next element 
+                                            false = filling the background color
+                                            index.php = linking the text with the other page or link. It is require for the social media account link purpose or redirect the user to the required pagex*/
+                                        $pdf->Cell(0, 20, "Registration Details", 1, 1, 'C');
+                                        $pdf->Cell(70, 15, "First Name", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $FirstName, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Middle Name", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $MiddleName, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Last Name", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $LastName, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Mobile No", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $Mobile, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, $Phone, 1, 0, 'C');
+                                        $pdf->Cell(0, 15, "First Name", 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Email", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $Email, 1, 1, 'C');
+
+
+                                        $pdf->Cell(70, 15, "Account Type", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $Account, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Status", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $Status, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Date of Birth(A.D)", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $DateAd, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Date of Birth(B.S)", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $DateBS, 1, 1, 'C');
+
+                                        $pdf->Cell(70, 15, "Country", 1, 0, 'C');
+                                        $pdf->Cell(0, 15, $Country, 1, 1, 'C');
+                                        $pdf->Cell(70, 15, "Owner : ", 0, 0, 'C');
+                                        $pdf->Cell(25, 15, "Sushant Khadka", 0, 1, 'C', false, 'https://www.instagram.com/sushant.exe_/');
+                                        // $pdf->output("$FirstName".".pdf");
+                                        $pdf->output();
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+                    }
+                } else {
+                    header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+                }
+            } else {
+                header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+            }
+        } else {
+            header('location:http://'.$_SERVER['HTTP_HOST']. '/php/FirstTry/index.php');
+        }
+    }
+}
